3aac5a839a778fd42b87a07d15ca7ef7933b79e3
mamalmaleki <mohammadalimalekie@gmail.com>
2023-03-03 19:51:36 +0330

    entry of the program

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+var build = "develop"
+
+func main() {
+	log.Println("starting service", build)
+	defer log.Println("service ended")
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+	<-shutdown
+
+	log.Println("stopping serviice")
+}
+
+//https://github.com/ardanlabs/service
+//https://github.com/ardanlabs/service/tree/service3
